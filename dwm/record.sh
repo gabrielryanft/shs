44a727131cd94d56bc2717f48f4cd2eb0dcc7114
Gabriel Ryan <gabrielryanf.t@gmail.com>
2025-06-20 17:53:35 -0300

    change the string sent to the bar(file)

--- dwm/record.sh
+++ dwm/record.sh
@@ -2,6 +2,7 @@
 
 if tmux has-session -t 'Recording'; then
 	tmux send-keys -t 'Recording'.1 'q'
+	tmux send-keys -t 'Recording'.0 'q'
 	echo "" > $HOME/settings/.recording.info
 	exit
 fi
@@ -9,7 +10,7 @@ fi
 curr_last_file="$HOME/Videos/Recordings/$(ls $HOME/Videos/Recordings/ -1 | tail -n 1)"
 
 sleep 1
-echo "| #Rec |" > $HOME/settings/.recording.info
+echo '| #Rec ' > $HOME/settings/.recording.info
 tmux new-session -d -s "Recording" \
 ffmpeg -f pulse -i "$(pactl list sources | grep Name | grep -i output | cut -d" " -f 2)" -f x11grab -i :0.0 "$HOME/Videos/Recordings/$(date +"%Y-%m-%d-%H%M%S_ffmpeg.mkv")"
 
